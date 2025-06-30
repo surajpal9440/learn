44c0c877605a79f65417a42824609e215d55874e
Suraj Pal <surajpal7875@gmail.com>
2025-06-30 12:42:52 +0530

    sec commit

--- day 2/first.js
+++ day 2/first.js
@@ -2,17 +2,6 @@ const name =" suraj ";
 const age=20;
 let balance =40393;
 
-<<<<<<< HEAD
-const account =4334443; //second commit
-
-function updatebalance(){  //third commit
-    balance+=20;
-}
-
-console.log("hello coder army"); //fourth commit
-
-const id=33;
-console.log(id); //fifth  commit
-=======
 const account =4334443;
->>>>>>> parent of 4f2084b (third commit)
+
+console.log("hello bhaiya");

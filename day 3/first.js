1614bf851bac49210a1ba1bffe21b7be54e7af24
Suraj Pal <surajpal7875@gmail.com>
2025-06-30 16:18:22 +0530

    upi inserted

--- day 3/first.js
+++ day 3/first.js
@@ -8,3 +8,9 @@ const footer ="footer added";
 console.log(footer);
 const payment = "payment added";
 console.log(payment);
+<<<<<<< HEAD
+=======
+
+const upi="integrated upi";
+console.log(upi);
+>>>>>>> a69847b (upi added)
